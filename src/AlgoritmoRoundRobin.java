9833fc7bb6016614243db9b1d4389609e11fe653
Heli de Souza Oliveira <heli.souza@ufms.br>
2026-04-23 21:27:12 -0400

    Remove improvement comment in Round Robin

--- src/AlgoritmoRoundRobin.java
+++ src/AlgoritmoRoundRobin.java
@@ -147,7 +147,6 @@ public class AlgoritmoRoundRobin implements Escalonador {
         System.out.printf("%-10s %-10s %-10s %-12s %-12s %-12s %-10s%n",
                 "Processo", "Chegada", "Burst", "Início", "Fim", "Espera", "Retorno");
 
-        // 🔥 melhoria opcional (igual ao SJF)
         processos.sort(Comparator.comparingInt(Processo::getInicio));
 
         double somaEspera = 0, somaRetorno = 0;
